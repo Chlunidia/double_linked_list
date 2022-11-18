e849e76466f9e4e24a92ba0f29e967911f65b78a
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-11-18 17:30:44 +0700

    Menambahkan method Search

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -58,6 +58,30 @@ namespace double_linked_list
                     return;
                 }
             }
+            /* On the execution above for loop, prev and
+             * current will point to those nodes
+             * between which the new node is to be inserted */
+            newNode.next = current;
+            newNode.prev = previous;
+            // If the node is to be inserted at the end of the list
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
+        }
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            previous = current = START;
+            while (current != null && rollNo != current.noMhs)
+            {
+                previous = current;
+                current = current.next;
+            }
+            return (current != null);       
         }
     }
     class Program
