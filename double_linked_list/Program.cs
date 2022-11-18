f37349b914f6e4b2c2dae885070bf1a4e33292c9
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-11-18 16:55:46 +0700

    Menambahkan class Node

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -2,6 +2,19 @@
 
 namespace double_linked_list
 {
+    class Node
+    {
+        /* Node class represent the node of doubly linked list
+         * It consist of the information part and links to 
+         * Its succending and precending
+         * in terms of next and previous */
+        public int noMhs;
+        public string name;
+        // Point to the succending node
+        public Node next;
+        // Point to the precending node
+        public Node prev;
+    }
     class Program
     {
         static void Main(string[] args)
